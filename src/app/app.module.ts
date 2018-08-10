e0b3bdaf8b2aa03794c29bebf5706078894ae486
ivan del campo <ividcr@gmail.com>
2018-08-10 14:03:28 +0200

    creacion de componentes  en angular , diseño del menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,21 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CabeceraComponent } from './cabecera/cabecera.component';
+import { PieComponent } from './pie/pie.component';
 
-//import {appRoutes} from  './app.routing';
-//import {RouterModule} from '@angular/router';
-//import{FormsModule} from '@angular/forms'
-//import{HttpModule} from '@angular/http'
+import {appRoutes} from  './app.routing';
+import {RouterModule} from '@angular/router';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
+import { LoginComponent } from './login/login.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CabeceraComponent,
+    PieComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    //FormsModule,
-    //HttpModule,
-    //RouterModule.forRoot(appRoutes)
+    FormsModule,
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,14 +1,13 @@
 import { Routes } from '@angular/router';
-//import {Component} from '';
-//import {Component} from '';
-//import {Component} from '';
+import {CabeceraComponent} from './cabecera/cabecera.component';
+import {PieComponent} from './pie/pie.component';
+import {LoginComponent} from './login/login.component';
 //import {Component} from '';
 //import {Component} from '';
 
 export const appRoutes: Routes = [
 {path: '', redirectTo: '', pathMatch: 'full'},
-//{path: '', component: Component},
-//{path: '', component: Component},
+{path: 'login', component:LoginComponent},
 //{path: '', component: Component},
 //{path: '', component: Component},
 //{path: '**', component: Component}
